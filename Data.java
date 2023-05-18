92f39bacab207f8063953ac9421c4515feee0c0f
singlechracter <133970345+singlechracter@users.noreply.github.com>
2023-05-18 19:38:36 +0800

    提交测试

---
+++ Data.java
@@ -0,0 +1,304 @@
+/**
+  * Copyright 2023 json.cn 
+  */
+package cn.json.pojo;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Auto-generated: 2023-04-23 9:11:36
+ *
+ * @author json.cn (i@json.cn)
+ * @website http://www.json.cn/java2pojo/
+ */
+public class Data {
+
+    private boolean adminAdd;
+    private String apkLink;
+    private int audit;
+    private String author;
+    private boolean canEdit;
+    private int chapterId;
+    private String chapterName;
+    private boolean collect;
+    private int courseId;
+    private String desc;
+    private String descMd;
+    private String envelopePic;
+    private boolean fresh;
+    private String host;
+    private int id;
+    private boolean isAdminAdd;
+    private String link;
+    private Date niceDate;
+    private Date niceShareDate;
+    private String origin;
+    private String prefix;
+    private String projectLink;
+    private long publishTime;
+    private int realSuperChapterId;
+    private boolean route;
+    private int selfVisible;
+    private long shareDate;
+    private String shareUser;
+    private int superChapterId;
+    private String superChapterName;
+    private List<String> tags;
+    private String title;
+    private int type;
+    private int userId;
+    private int visible;
+    private int zan;
+    public void setAdminAdd(boolean adminAdd) {
+         this.adminAdd = adminAdd;
+     }
+     public boolean getAdminAdd() {
+         return adminAdd;
+     }
+
+    public void setApkLink(String apkLink) {
+         this.apkLink = apkLink;
+     }
+     public String getApkLink() {
+         return apkLink;
+     }
+
+    public void setAudit(int audit) {
+         this.audit = audit;
+     }
+     public int getAudit() {
+         return audit;
+     }
+
+    public void setAuthor(String author) {
+         this.author = author;
+     }
+     public String getAuthor() {
+         return author;
+     }
+
+    public void setCanEdit(boolean canEdit) {
+         this.canEdit = canEdit;
+     }
+     public boolean getCanEdit() {
+         return canEdit;
+     }
+
+    public void setChapterId(int chapterId) {
+         this.chapterId = chapterId;
+     }
+     public int getChapterId() {
+         return chapterId;
+     }
+
+    public void setChapterName(String chapterName) {
+         this.chapterName = chapterName;
+     }
+     public String getChapterName() {
+         return chapterName;
+     }
+
+    public void setCollect(boolean collect) {
+         this.collect = collect;
+     }
+     public boolean getCollect() {
+         return collect;
+     }
+
+    public void setCourseId(int courseId) {
+         this.courseId = courseId;
+     }
+     public int getCourseId() {
+         return courseId;
+     }
+
+    public void setDesc(String desc) {
+         this.desc = desc;
+     }
+     public String getDesc() {
+         return desc;
+     }
+
+    public void setDescMd(String descMd) {
+         this.descMd = descMd;
+     }
+     public String getDescMd() {
+         return descMd;
+     }
+
+    public void setEnvelopePic(String envelopePic) {
+         this.envelopePic = envelopePic;
+     }
+     public String getEnvelopePic() {
+         return envelopePic;
+     }
+
+    public void setFresh(boolean fresh) {
+         this.fresh = fresh;
+     }
+     public boolean getFresh() {
+         return fresh;
+     }
+
+    public void setHost(String host) {
+         this.host = host;
+     }
+     public String getHost() {
+         return host;
+     }
+
+    public void setId(int id) {
+         this.id = id;
+     }
+     public int getId() {
+         return id;
+     }
+
+    public void setIsAdminAdd(boolean isAdminAdd) {
+         this.isAdminAdd = isAdminAdd;
+     }
+     public boolean getIsAdminAdd() {
+         return isAdminAdd;
+     }
+
+    public void setLink(String link) {
+         this.link = link;
+     }
+     public String getLink() {
+         return link;
+     }
+
+    public void setNiceDate(Date niceDate) {
+         this.niceDate = niceDate;
+     }
+     public Date getNiceDate() {
+         return niceDate;
+     }
+
+    public void setNiceShareDate(Date niceShareDate) {
+         this.niceShareDate = niceShareDate;
+     }
+     public Date getNiceShareDate() {
+         return niceShareDate;
+     }
+
+    public void setOrigin(String origin) {
+         this.origin = origin;
+     }
+     public String getOrigin() {
+         return origin;
+     }
+
+    public void setPrefix(String prefix) {
+         this.prefix = prefix;
+     }
+     public String getPrefix() {
+         return prefix;
+     }
+
+    public void setProjectLink(String projectLink) {
+         this.projectLink = projectLink;
+     }
+     public String getProjectLink() {
+         return projectLink;
+     }
+
+    public void setPublishTime(long publishTime) {
+         this.publishTime = publishTime;
+     }
+     public long getPublishTime() {
+         return publishTime;
+     }
+
+    public void setRealSuperChapterId(int realSuperChapterId) {
+         this.realSuperChapterId = realSuperChapterId;
+     }
+     public int getRealSuperChapterId() {
+         return realSuperChapterId;
+     }
+
+    public void setRoute(boolean route) {
+         this.route = route;
+     }
+     public boolean getRoute() {
+         return route;
+     }
+
+    public void setSelfVisible(int selfVisible) {
+         this.selfVisible = selfVisible;
+     }
+     public int getSelfVisible() {
+         return selfVisible;
+     }
+
+    public void setShareDate(long shareDate) {
+         this.shareDate = shareDate;
+     }
+     public long getShareDate() {
+         return shareDate;
+     }
+
+    public void setShareUser(String shareUser) {
+         this.shareUser = shareUser;
+     }
+     public String getShareUser() {
+         return shareUser;
+     }
+
+    public void setSuperChapterId(int superChapterId) {
+         this.superChapterId = superChapterId;
+     }
+     public int getSuperChapterId() {
+         return superChapterId;
+     }
+
+    public void setSuperChapterName(String superChapterName) {
+         this.superChapterName = superChapterName;
+     }
+     public String getSuperChapterName() {
+         return superChapterName;
+     }
+
+    public void setTags(List<String> tags) {
+         this.tags = tags;
+     }
+     public List<String> getTags() {
+         return tags;
+     }
+
+    public void setTitle(String title) {
+         this.title = title;
+     }
+     public String getTitle() {
+         return title;
+     }
+
+    public void setType(int type) {
+         this.type = type;
+     }
+     public int getType() {
+         return type;
+     }
+
+    public void setUserId(int userId) {
+         this.userId = userId;
+     }
+     public int getUserId() {
+         return userId;
+     }
+
+    public void setVisible(int visible) {
+         this.visible = visible;
+     }
+     public int getVisible() {
+         return visible;
+     }
+
+    public void setZan(int zan) {
+         this.zan = zan;
+     }
+     public int getZan() {
+         return zan;
+     }
+
+}
